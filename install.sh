5ad95bdcabcbbd4ca1be62a62e924e3e31cf2cd8
Bendik Eger <bendik.eger@spacenorway.com>
2024-09-04 10:07:35 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+packages=(
+	stow
+	lazygit
+	nvim
+	tmux
+)
+
+# Iterate over the array and install each package
+for package in "${packages[@]}"; do
+	echo "Installing $package..."
+	/home/linuxbrew/.linuxbrew/bin/brew install "$package"
+done
+
+echo "All packages have been installed."
+
+# Set up XDG_CONFIG_HOME
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"
+
+# Create symlinks for existing configurations
+stow nvim
+stow tmux
+stow zsh
+
+echo "Setup complete."
