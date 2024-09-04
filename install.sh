5b5346584657e9ed8a1be3fe630fda4a6ed1d773
Bendik Eger <bendik.eger@spacenorway.com>
2024-09-04 15:05:47 +0200

    Tweak install sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 packages=(
 	zsh
+	fzf
 	stow
 	lazygit
 	nvim
@@ -14,8 +15,7 @@ for package in "${packages[@]}"; do
 	/home/linuxbrew/.linuxbrew/bin/brew install "$package"
 done
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "All packages have been installed."
 
@@ -24,8 +24,7 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 # Create symlinks for existing configurations
-stow nvim
-stow tmux
-stow zsh
+stow --adopt *
+git restore .
 
 echo "Setup complete."
